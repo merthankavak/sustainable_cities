84d578ebcd6fbf319f2bdb36d5ad5aa78fcca2b7
Efe <jimmyfallon717@gmail.com>
2021-02-06 18:58:55 +0300

    hotfix.

--- lib/UI/landingPage.dart
+++ lib/UI/landingPage.dart
@@ -30,11 +30,7 @@ class _LandingPageState extends State<LandingPage> {
           child: Text(
             "EARTHQUAKE QUIZ",
             textAlign: TextAlign.center,
-<<<<<<< HEAD
             style: TextStyle(color: Colors.white, fontSize: 22),
-=======
-            style: TextStyle(color: Colors.white),
->>>>>>> 850db822dcf41d8de261a31656e0d41c05e8d96c
           ),
         ),
       ),
@@ -54,15 +50,9 @@ class _LandingPageState extends State<LandingPage> {
         ),
         child: Center(
           child: Text(
-<<<<<<< HEAD
             "EARTHQUAKE TEST",
             textAlign: TextAlign.center,
             style: TextStyle(color: Colors.white, fontSize: 22),
-=======
-            "",
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.white),
->>>>>>> 850db822dcf41d8de261a31656e0d41c05e8d96c
           ),
         ),
       ),
@@ -84,11 +74,7 @@ class _LandingPageState extends State<LandingPage> {
           child: Text(
             "EARTHQUAKE INFORMATION",
             textAlign: TextAlign.center,
-<<<<<<< HEAD
             style: TextStyle(color: Colors.white, fontSize: 22),
-=======
-            style: TextStyle(color: Colors.white),
->>>>>>> 850db822dcf41d8de261a31656e0d41c05e8d96c
           ),
         ),
       ),
@@ -110,11 +96,7 @@ class _LandingPageState extends State<LandingPage> {
           child: Text(
             "ABOUT",
             textAlign: TextAlign.center,
-<<<<<<< HEAD
             style: TextStyle(color: Colors.white, fontSize: 22),
-=======
-            style: TextStyle(color: Colors.white, fontSize: 25),
->>>>>>> 850db822dcf41d8de261a31656e0d41c05e8d96c
           ),
         ),
       ),
@@ -132,7 +114,6 @@ class _LandingPageState extends State<LandingPage> {
         inAsyncCall: _loadingVisible,
         child: Center(
           child: GridView.count(
-<<<<<<< HEAD
             padding: EdgeInsets.all(14.0),
             shrinkWrap: true,
             mainAxisSpacing: 15,
@@ -140,14 +121,7 @@ class _LandingPageState extends State<LandingPage> {
             crossAxisCount: 2,
             childAspectRatio: 0.65,
             children: <Widget>[
-              gridViewElement,
-=======
-            mainAxisSpacing: 5,
-            crossAxisSpacing: 5,
-            crossAxisCount: 2,
-            children: <Widget>[
               gridViewElement1,
->>>>>>> 850db822dcf41d8de261a31656e0d41c05e8d96c
               gridViewElement2,
               gridViewElement3,
               gridViewElement4
